2d44be796faca6a37bd7f583cf05c5cbaddb8602
김재형 <wogud2437@naver.com>
2025-01-15 16:39:55 +0900

    Thread 생성해보기 및 기초개념 이해

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,26 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        int size = 100000;
+        MyThread thread = new MyThread();
+        thread.start();
 
-        // ArrayList 성능 테스트
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.ensureCapacity(5000);
-        long startTime = System.nanoTime();
-        for(int i = 0; i < size; i++) {
-            arrayList.add(i);
-        }
-        long endTime = System.nanoTime();
-        System.out.println("ArrayList 추가 시간: " + (endTime - startTime) / 1_000_000 + " ms");
+        Thread thread1 = new Thread(new MyRunnable());
+        thread1.start();
 
-        // LinkedList 성능 테스트
-        List<Integer> linkedList = new LinkedList<>();
-        startTime = System.nanoTime();
-        for(int i = 0; i < size; i++) {
-            linkedList.add(i);
-        }
-        endTime = System.nanoTime();
-        System.out.println("LinkedList 추가 시간: " + (endTime - startTime) / 1_000_000 + " ms");
+        MyRunnable myRunnable = new MyRunnable();
+        myRunnable.run();
+    }
 
-        // ArrayList 조회 시간
-        startTime = System.nanoTime();
-        for(int i = 0; i < size; i++) {
-            arrayList.get(i);
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Thread 실행");
         }
-        endTime = System.nanoTime();
-        System.out.println("ArrayList 조회 시간: " + (endTime - startTime) / 1_000_000 + " ms");
-
-        // LinkedList 조회 시간
-        startTime = System.nanoTime();
-        for(int i = 0; i < size; i++) {
-            linkedList.get(i);
+    }
+    private static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            // 실행할 코드
+            System.out.println("Runnable 스레드 실행 중...");
         }
-        endTime = System.nanoTime();
-        System.out.println("LinkedList 조회 시간: " + (endTime - startTime) / 1_000_000 + " ms");
-
-        // ArrayList 삭제 시간 (중간 삭제)
-        startTime = System.nanoTime();
-        for(int i = size - 1; i >= 0; i--) {
-            arrayList.remove(i);
-        }
-        endTime = System.nanoTime();
-        System.out.println("ArrayList 삭제 시간: " + (endTime - startTime) / 1_000_000 + " ms");
-
-        // LinkedList 삭제 시간 (중간 삭제)
-        startTime = System.nanoTime();
-        for(int i = size - 1; i >= 0; i--) {
-            linkedList.remove(i);
-        }
-        endTime = System.nanoTime();
-        System.out.println("LinkedList 삭제 시간: " + (endTime - startTime) / 1_000_000 + " ms");
     }
 }
