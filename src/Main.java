f61e1470e2ebfef4bb17bf66cd870cd96a59d1b2
김재형 <wogud2437@naver.com>
2025-01-21 19:45:31 +0900

    백준 9012 / 괄호

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        MyThread thread = new MyThread();
-        thread.start();
+        // 백준 9012, 괄호 문제
+        // 괄호 문자열(Parenthesis String, PS)은 두 개의 괄호 기호인 ‘(’ 와 ‘)’ 만으로 구성되어 있는 문자열이다.
+        // 그 중에서 괄호의 모양이 바르게 구성된 문자열을 올바른 괄호 문자열(Valid PS, VPS)이라고 부른다. 한 쌍의 괄호 기호로 된 “( )” 문자열은 기본 VPS 이라고 부른다.
+        // 만일 x 가 VPS 라면 이것을 하나의 괄호에 넣은 새로운 문자열 “(x)”도 VPS 가 된다. 그리고 두 VPS x 와 y를 접합(concatenation)시킨 새로운 문자열 xy도 VPS 가 된다.
+        // 예를 들어 “(())()”와 “((()))” 는 VPS 이지만 “(()(”, “(())()))” , 그리고 “(()” 는 모두 VPS 가 아닌 문자열이다.
+        // 여러분은 입력으로 주어진 괄호 문자열이 VPS 인지 아닌지를 판단해서 그 결과를 YES 와 NO 로 나타내어야 한다.
 
-        Thread thread1 = new Thread(new MyRunnable());
-        thread1.start();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
-        MyRunnable myRunnable = new MyRunnable();
-        myRunnable.run();
-    }
-
-    public static class MyThread extends Thread {
-        @Override
-        public void run() {
-            System.out.println("Thread 실행");
+        for (int i=0; i<num; i++){
+            String str = sc.next();
+            if (validStr(str))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
     }
-    private static class MyRunnable implements Runnable {
-        @Override
-        public void run() {
-            // 실행할 코드
-            System.out.println("Runnable 스레드 실행 중...");
+
+    public static boolean validStr(String str){
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char chr:str.toCharArray()){
+            if (chr == '('){
+                stack.push(chr);
+            }else if (chr == ')'){
+                if (stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
         }
+        return stack.isEmpty();
     }
 }
