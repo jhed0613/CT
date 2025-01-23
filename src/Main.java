c972050b56ac3f44a77efae2e0f5bd2c3a808c3c
김재형 <wogud2437@naver.com>
2025-01-23 19:21:11 +0900

    백준 9935 / 문자열 폭발 / BufferedReader 로 구현

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,17 @@ import java.util.Scanner;
 //        상근이는 모든 폭발이 끝난 후에 어떤 문자열이 남는지 구해보려고 한다. 남아있는 문자가 없는 경우가 있다. 이때는 "FRULA"를 출력한다.
 //        폭발 문자열은 같은 문자를 두 개 이상 포함하지 않는다.
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String strArr = sc.nextLine();
-        String c4 = sc.nextLine();
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        // BufferedReader를 사용하여 입력을 받습니다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String strArr = br.readLine();
+        String c4 = br.readLine();
+        br.close();
 
         StringBuilder sb = new StringBuilder();
         int bombLength = c4.length();
