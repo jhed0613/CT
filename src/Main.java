89af53ee3b261b0418637bfd86931a27e26a49cb
김재형 <wogud2437@naver.com>
2025-01-23 17:13:13 +0900

    백준 9012 / 괄호

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.sql.Array;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
