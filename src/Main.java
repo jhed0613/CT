e90112eaa0f72a32d4f0b0aebff8f06fc5efb1f0
김재형 <wogud2437@naver.com>
2025-01-26 23:38:54 +0900

    김수민 편지씀;

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,18 @@ public class Main {
         String strNum = br.readLine();
         int num = Integer.parseInt(strNum);
 
-        String strNums = br.readLine();
-        String[] strNumList = strNums.split(" ");
-
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
+        int[][] triangle = new int[num][num];
+        int[][] dp = new int[num][num];
 
         for (int i=0; i<num; i++){
-            int numList = Integer.parseInt(strNumList[i]);
-            max = numList > max ? numList : max;
-            min = numList < min ? numList : min;
+            for (int j=0; j<=i; j++){
+                int a = Integer.parseInt(br.readLine());
+                triangle[i][j] = a;
+                안녕ㅎㅅㅇ ㄱㅂ 열심히 해헤헤 이거 다 ㅁㅎㄹ ㅋㅋㅋ
+                        이거 왜 띄어쓰기가 이래 별루다 ㅜ
+                        들여쓰기 나도 알아 이자식아
+                        암튼 아 좋은 말 쓸라햇는데 너가 그만 쓰라해서 안 쓴다 ㅃㅇ==
+            }
         }
-        System.out.println(min + " " + max);
     }
 }
