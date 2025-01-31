f50013be02e10d22998837ee77c2526e550f667d
김재형 <wogud2437@naver.com>
2025-01-31 17:10:40 +0900

    백준 1010 / 다리놓기 / combination (조합) 메서드 수정

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    // 조합을 계산하는 함수
+    public static long combination(int m, int n) {
+        if(n > m - n) n = m - n; // C(m,n) == C(m, m-n)
+        long result = 1;
+        for(int i = 1; i <= n; i++) {
+            result = result * (m - n + i) / i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String t = br.readLine();
+        int testCase = Integer.parseInt(t.trim());
 
-        int testCase = Integer.parseInt(t);
-
-        for (int i=0; i<testCase; i++){
+        for (int i = 0; i < testCase; i++) {
             String a = br.readLine();
-            String[] split = a.split(" ");
+            String[] split = a.trim().split("\\s+");
 
             int n = Integer.parseInt(split[0]);
             int m = Integer.parseInt(split[1]);
 
-            long totalN = 1;
-            long totalM = 1;
-            long answer;
-
-            int size = n;
-            int cSize = 1;
-
-            for (int j=0; j<size; j++){
-                totalN *= cSize++;
-                totalM *= m--;
-            }
-
-            answer = totalM / totalN;
+            long answer = combination(m, n);
             System.out.println(answer);
         }
     }
